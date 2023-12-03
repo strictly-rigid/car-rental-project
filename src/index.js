3a9bfb5ed7667c6417f5f66f644546e0a5ed7b19
strictly-rigid <holysociology@gmail.com>
2023-12-03 19:11:19 +0200

    add basename

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { persistor, store } from "./redux/store";
-import "./index.css";
-import App from "./components/App";
-import reportWebVitals from "./reportWebVitals";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { persistor, store } from './redux/store';
+import './index.css';
+import App from './components/App';
+import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/car-rental-project">
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
